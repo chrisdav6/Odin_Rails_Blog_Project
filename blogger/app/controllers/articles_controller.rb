cfc0fdd036f01fd063951982cc3bcd1afc0cc9f5
Chris Davis <chris.davis5440@gmail.com>
2016-08-02 16:12:24 -0400

    Configured comments

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -7,6 +7,9 @@ class ArticlesController < ApplicationController
 
 	def show
 		@article = Article.find(params[:id])
+
+		@comment = Comment.new
+		@comment.article_id = @article.id
 	end
 
 	def new
@@ -35,7 +38,7 @@ class ArticlesController < ApplicationController
 		@article.update(article_params)
 
 		flash.notice = "Article '#{@article.title}' Updated!"
-		
+
 		redirect_to article_path(@article)
 	end
 
